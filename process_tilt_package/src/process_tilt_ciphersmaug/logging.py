2df6cb3a08a31b0185fa0bcdd3733b6fbff2aec8
ciphersmaug <39407479+ciphersmaug@users.noreply.github.com>
2023-03-17 14:29:26 +0100

    implemented trace input

--- process_tilt_package/src/process_tilt_ciphersmaug/logging.py
+++ process_tilt_package/src/process_tilt_ciphersmaug/logging.py
@@ -14,7 +14,7 @@ TILT_LOG_DEFAULTS = {
 }
 
 class TiltLogger:
-    def __init__(self,log_name) -> None:
+    def __init__(self,log_name,tracer = None) -> None:
         self.logger : logging.Logger = logging.getLogger(log_name)
         formatter = logging.Formatter(fmt=TILT_LOG_FORMAT, defaults=TILT_LOG_DEFAULTS,
                                   datefmt=logging.Formatter.default_msec_format)
@@ -22,7 +22,10 @@ class TiltLogger:
         stream_handler.setFormatter(formatter)
         self.logger.addHandler(stream_handler)
         self.logger.setLevel(logging.INFO)
-        self.tracer = trace.get_tracer(__name__)
+        if tracer:
+            self.tracer = tracer
+        else:
+            self.tracer = trace.get_tracer(__name__)
 
     def log(self,concept_name: str,tilt: dict, msg: str ,level: int = logging.INFO):
         def inner_func(original_function):
